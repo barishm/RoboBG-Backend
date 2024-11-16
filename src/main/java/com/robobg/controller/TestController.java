7dadd9ff2750cbbf32937cd86ea4f6c2b74f93e0
barishm <barishm@outlook.com>
2024-11-16 20:50:46 +0200

    testing pipeline

--- src/main/java/com/robobg/controller/TestController.java
+++ src/main/java/com/robobg/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.robobg.controller;
 
 
-import com.robobg.entity.dtos.RobotDTO.RobotModelImageLinksDTO;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +10,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/test")
+@RequestMapping("/")
 public class TestController {
     @GetMapping()
     public String returnsHelloWorld(){
