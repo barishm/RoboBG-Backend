03c3f14d496bbf120d9bd0ac96bb5c2087a574c6
Barish Mehmed <barishm@outlook.com>
2025-06-03 20:57:51 +0300

    cross origin for www

--- src/main/java/com/robobg/config/SecurityConfig.java
+++ src/main/java/com/robobg/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://barishm.com", "http://barishm.com"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://barishm.com", "http://barishm.com","http://www.barishm.com","https://www.barishm.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
